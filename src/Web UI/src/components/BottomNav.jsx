3ad274c443486ab27e3369cbdbdf6960d62e170c
Astijus Arimavičius <astijus.arimavicius@gmail.com>
2023-11-02 20:37:44 +0200

    Add top shadow to bottom nav

--- src/Web UI/src/components/BottomNav.jsx
+++ src/Web UI/src/components/BottomNav.jsx
@@ -4,8 +4,8 @@ const BottomNav = () => {
     const [activeTab, setActiveTab] = useState("profile");
 
     return (
-        <div className="btm-nav w-full md:mx-auto md:w-4/5 lg:w-3/5 xl:w-1/2">
-            <button className={`${activeTab === "profile" ? "active" : ""} bg-neutral rounded-tl-lg`}
+        <div className="btm-nav w-full shadow-[black_0px_-5px_5px_-5px] md:mx-auto md:w-4/5 lg:w-3/5 xl:w-1/2">
+            <button className={`${activeTab === "profile" ? "active" : ""} bg-neutral rounded-bl-2xl`}
                 onClick={() => setActiveTab("profile")}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6"><path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" /></svg>
                 <span className="btm-nav-label">Profile</span>
@@ -20,7 +20,7 @@ const BottomNav = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6"><path fillRule="evenodd" d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z" clipRule="evenodd" /></svg>
                 <span className="btm-nav-label">Workout</span>
             </button>
-            <button className={`${activeTab === "exercises" ? "active" : ""} bg-neutral rounded-tr-lg`}
+            <button className={`${activeTab === "exercises" ? "active" : ""} bg-neutral rounded-br-2xl`}
                 onClick={() => setActiveTab("exercises")}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6"><path d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375z" /><path fillRule="evenodd" d="M3.087 9l.54 9.176A3 3 0 006.62 21h10.757a3 3 0 002.995-2.824L20.913 9H3.087zm6.163 3.75A.75.75 0 0110 12h4a.75.75 0 010 1.5h-4a.75.75 0 01-.75-.75z" clipRule="evenodd" /></svg>
                 <span className="btm-nav-label">Exercises</span>
