62c7277dc08cb4753e56f34abe050f07f874b6e2
Astijus Arimavičius <astijus.arimavicius@gmail.com>
2023-11-04 11:21:09 +0200

    refactor bottom nav

--- src/Web UI/src/components/BottomNav.jsx
+++ src/Web UI/src/components/BottomNav.jsx
@@ -1,48 +1,28 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const BottomNav = () => {
-    const [activeTab, setActiveTab] = useState("profile");
     const navigate = useNavigate();
-
-    const onClick = (tab) => {
-        setActiveTab(tab);
-
-        switch (tab) {
-            case "profile":
-                navigate("/profile");
-                break;
-            case "history":
-                navigate("/history");
-                break;
-            case "workout":
-                navigate("/workout");
-                break;
-            case "exercises":
-                navigate("/exercises");
-                break;
-        }
-    }
+    let location = useLocation();
 
     return (
         <div className="btm-nav w-full shadow-[black_0px_-5px_5px_-5px] sm:mx-auto sm:w-2/3 md:w-3/5 lg:w-3/6 xl:w-1/3">
-            <button className={`${activeTab === "profile" ? "active" : ""} bg-neutral sm:rounded-bl-2xl`}
-                onClick={() => onClick("profile")}>
+            <button className={`${location.pathname.slice(1) === "profile" ? "active" : ""} bg-neutral sm:rounded-bl-2xl`}
+                onClick={() => navigate("/profile")}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6"><path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" /></svg>
                 <span className="btm-nav-label">Profile</span>
             </button>
-            <button className={`${activeTab === "history" ? "active" : ""} bg-neutral`}
-                onClick={() => onClick("history")}>
+            <button className={`${location.pathname.slice(1) === "history" ? "active" : ""} bg-neutral`}
+                onClick={() => navigate("/history")}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6"><path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z" clipRule="evenodd" /></svg>
                 <span className="btm-nav-label">History</span>
             </button>
-            <button className={`${activeTab === "workout" ? "active" : ""} bg-neutral`}
-                onClick={() => onClick("workout")}>
+            <button className={`${location.pathname.slice(1) === "workout" ? "active" : ""} bg-neutral`}
+                onClick={() => navigate("/workout")}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6"><path fillRule="evenodd" d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z" clipRule="evenodd" /></svg>
                 <span className="btm-nav-label">Workout</span>
             </button>
-            <button className={`${activeTab === "exercises" ? "active" : ""} bg-neutral sm:rounded-br-2xl`}
-                onClick={() => onClick("exercises")}>
+            <button className={`${location.pathname.slice(1) === "exercises" ? "active" : ""} bg-neutral sm:rounded-br-2xl`}
+                onClick={() => navigate("/exercises")}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6"><path d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375z" /><path fillRule="evenodd" d="M3.087 9l.54 9.176A3 3 0 006.62 21h10.757a3 3 0 002.995-2.824L20.913 9H3.087zm6.163 3.75A.75.75 0 0110 12h4a.75.75 0 010 1.5h-4a.75.75 0 01-.75-.75z" clipRule="evenodd" /></svg>
                 <span className="btm-nav-label">Exercises</span>
             </button>
