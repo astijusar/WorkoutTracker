ed55f3ef4519b5804adec058f3e9f559e012e6a4
Astijus Arimavičius <astijus.arimavicius@gmail.com>
2023-11-15 18:19:36 +0200

    Update authSlice to get roles

--- src/Web UI/src/auth/authSlice.js
+++ src/Web UI/src/auth/authSlice.js
@@ -1,19 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { jwtDecode } from "jwt-decode";
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState: { user: null, accessToken: null, refreshToken: null },
+    name: "auth",
+    initialState: {
+        user: null,
+        accessToken: null,
+        refreshToken: null,
+        roles: null,
+    },
     reducers: {
         setCredentials: (state, action) => {
-            const { user, accessToken, refreshToken } = action.payload
-            state.user = user,
-            state.accessToken = accessToken,
-            state.refreshToken = refreshToken
+            const { user, accessToken, refreshToken } = action.payload;
+            state.user = user;
+            state.accessToken = accessToken;
+            state.refreshToken = refreshToken;
+            state.roles =
+                jwtDecode(accessToken)[
+                    "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
+                ];
         },
         logOut: (state, action) => {
-            state.user = null,
-            state.accessToken = null
-            state.refreshToken = null
+            state.user = null;
+            state.accessToken = null;
+            state.refreshToken = null;
+            state.roles = null;
         },
     },
 });
@@ -25,3 +36,4 @@ export default authSlice.reducer;
 export const selectCurrentUser = (state) => state.auth.user;
 export const selectCurrentToken = (state) => state.auth.accessToken;
 export const selectCurrentRefreshToken = (state) => state.auth.refreshToken;
+export const selectCurrentUserRoles = (state) => state.auth.roles;
