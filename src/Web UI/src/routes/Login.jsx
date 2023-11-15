78d09c286e8230b5ba29a90d52107a956ec7bf82
Astijus Arimavičius <astijus.arimavicius@gmail.com>
2023-11-15 17:53:24 +0200

    Refactor login page

--- src/Web UI/src/routes/Login.jsx
+++ src/Web UI/src/routes/Login.jsx
@@ -1,83 +1,143 @@
-import { useRef, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router';
-import { useDispatch } from 'react-redux';
-import { setCredentials } from '../auth/authSlice';
-import { useLoginMutation } from '../auth/authApiSlice';
+import { useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+import { motion } from "framer-motion";
+import { setCredentials } from "../auth/authSlice";
+import { useLoginMutation } from "../auth/authApiSlice";
+import { useState } from "react";
 
 const Login = () => {
-    const userNameRef = useRef();
-    const errRef = useRef();
-    const [userName, setUserName] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [errMsg, setErrMsg] = useState('');
-    const navigate = useNavigate();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setError,
+    } = useForm();
 
+    const [apiError, setApiError] = useState("");
+    const navigate = useNavigate();
     const [login, { isLoading }] = useLoginMutation();
     const dispatch = useDispatch();
 
-    useEffect(() => {
-        userNameRef.current.focus();
-    }, []);
-
-    useEffect(() => {
-        setErrMsg('');
-    }, [userName, pwd]);
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
+    const onSubmit = async (data) => {
         try {
-            const userData = await login({ userName, password: pwd }).unwrap();
-            dispatch(setCredentials({ ...userData, userName }))
-            setUserName('');
-            setPwd('');
-            navigate('/profile');
+            const userData = await login({
+                username: data.username,
+                password: data.password,
+            }).unwrap();
+            dispatch(setCredentials({ ...userData, userName }));
+            navigate("/profile");
         } catch (err) {
-            if (!err?.response) {
-                setErrMsg('No Server Response');
+            console.log(err);
+            if (!err?.response && err?.status === "FETCH_ERROR") {
+                setApiError("Unable to reach server. Please try again later!");
             } else if (err.response?.status === 400) {
-                setErrMsg('Missing Username or Password');
+                setError("username", {
+                    type: "manual",
+                    message: "Username is required!",
+                });
+                setError("password", {
+                    type: "manual",
+                    message: "Password is required!",
+                });
             } else if (err.response?.status === 401) {
-                setErrMsg('Unauthorized');
+                setApiError("Username or password is incorrect!");
             } else {
-                setErrMsg('Login Failed');
+                setApiError("Please try again later!");
             }
-
-            errRef.current.focus();
         }
-    }
+    };
 
-    const handleUserNameInput = (e) => setUserName(e.target.value);
-    const handlePwdInput = (e) => setPwd(e.target.value);
-
-    const content = isLoading ? <h1>Loading...</h1> : (
-        <>
-            <p ref={errRef} className={errMsg ? "block" : "hidden"}>{errMsg}</p>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor='username'>Username:</label>
-                <input 
-                    type='text'
-                    id='username'
-                    ref={userNameRef}
-                    value={userName}
-                    onChange={handleUserNameInput}
-                    required
-                />
-                <label htmlFor='password'>Password:</label>
-                <input 
-                    type='password'
-                    id='password'
-                    onChange={handlePwdInput}
-                    value={pwd}
-                    required
-                />
-                <button>Sign In</button>
-            </form>
-        </>
+    const content = (
+        <div className="w-full min-h-screen flex justify-center items-center">
+            <div className="card mx-3 sm:mx-none bg-neutral md:mx-auto md:w-3/5 lg:w-2/4 xl:w-1/3">
+                <div className="card-body shadow-xl">
+                    <div className="card-title mb-2 text-3xl font-bold leading-tight tracking-tight">
+                        <h1>Sign in to your account</h1>
+                    </div>
+                    {apiError && (
+                        <motion.div
+                            className="text-error mb-1"
+                            initial={{ scale: 0.5, opacity: 0 }}
+                            animate={{ scale: 1, opacity: 1 }}
+                            exit={{ scale: 0.5, opacity: 0 }}
+                            transition={{ duration: 0.5 }}
+                        >
+                            {apiError}
+                        </motion.div>
+                    )}
+                    <form
+                        className="space-y-4 md:space-y-6"
+                        onSubmit={handleSubmit(onSubmit)}
+                    >
+                        <div>
+                            <label
+                                className="block mb-2 text-sm font-medium"
+                                htmlFor="username"
+                            >
+                                Username:
+                            </label>
+                            <input
+                                {...register("username", { required: true })}
+                                className="input w-full"
+                                type="text"
+                                id="username"
+                                required
+                            />
+                            {errors.username && (
+                                <p className="text-error mt-1">
+                                    {errors.username.message}
+                                </p>
+                            )}
+                        </div>
+                        <div>
+                            <label
+                                className="block mb-2 text-sm font-medium"
+                                htmlFor="password"
+                            >
+                                Password:
+                            </label>
+                            <input
+                                {...register("password", {
+                                    required: true,
+                                })}
+                                className="input w-full mb-1"
+                                type="password"
+                                id="password"
+                                required
+                            />
+                            {errors.password && (
+                                <p className="text-error mt-1">
+                                    {errors.password.message}
+                                </p>
+                            )}
+                        </div>
+                        <button
+                            className="btn btn-secondary text-neutral w-full disabled:opacity-75 disabled:bg-secondary disabled:text-neutral"
+                            type="submit"
+                            disabled={isLoading}
+                        >
+                            Sign In{" "}
+                            {isLoading && (
+                                <span className="loading loading-spinner loading-sm"></span>
+                            )}
+                        </button>
+                        <p className="text-sm font-light">
+                            Don’t have an account yet?{" "}
+                            <a
+                                href="#"
+                                className="font-medium text-secondary hover:underline "
+                            >
+                                Sign up
+                            </a>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 
     return content;
-}
+};
 
-export default Login
+export default Login;
