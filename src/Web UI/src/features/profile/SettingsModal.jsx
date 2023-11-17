50a2afce9ec4a52f796cda9a5beb04ea258e3cdb
Astijus Arimavičius <astijus.arimavicius@gmail.com>
2023-11-17 14:30:16 +0200

    Update profile page with settings modal

---
+++ src/Web UI/src/features/profile/SettingsModal.jsx
@@ -0,0 +1,35 @@
+import { useDispatch } from "react-redux";
+import { logOut } from "../auth/authSlice";
+
+const SettingsModal = ({ modalRef }) => {
+    const dispatch = useDispatch();
+
+    return (
+        <dialog ref={modalRef} className="modal">
+            <div className="modal-box">
+                <form method="dialog">
+                    <button className="btn btn-sm btn-circle btn-ghost p-3 absolute right-2 top-2">
+                        ✕
+                    </button>
+                </form>
+                <div className="flex flex-col gap-4">
+                    <h3 className="font-bold text-2xl">Settings</h3>
+                    <button className="btn btn-warning w-full">
+                        DELETE WORKOUT HISTORY
+                    </button>
+                    <button
+                        className="btn btn-error w-full"
+                        onClick={() => dispatch(logOut())}
+                    >
+                        LOG OUT
+                    </button>
+                </div>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+                <button>close</button>
+            </form>
+        </dialog>
+    );
+};
+
+export default SettingsModal;
