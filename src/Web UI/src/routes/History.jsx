18e8c16e0c2b2d9af1235d623ee3bb54e2e674d4
Astijus Arimavičius <astijus.arimavicius@gmail.com>
2023-11-23 20:22:52 +0200

    Update history page to handle no workouts

--- src/Web UI/src/routes/History.jsx
+++ src/Web UI/src/routes/History.jsx
@@ -13,29 +13,30 @@ const History = () => {
         isFetching,
     } = useGetWorkoutsQuery({ pageNumber: page, template: false });
 
+    const content =
+        workouts && workouts.length !== 0 ? (
+            <>
+                <div className="mt-7 flex justify-between">
+                    <h5 className="text-sm text-gray-400 tracking-widest">
+                        {moment(workouts[0].start).format("MMMM").toUpperCase()}
+                    </h5>
+                    <h5 className="text-sm text-gray-400">
+                        {pagination.totalCount}{" "}
+                        {pagination.totalCount === 1 ? "workout" : "workouts"}
+                    </h5>
+                </div>
+                <WorkoutHistoryList workouts={workouts} />
+            </>
+        ) : (
+            <h1 className="mt-10 text-3xl font-medium text-center">
+                No workouts completed!
+            </h1>
+        );
+
     return (
         <div className="mx-5">
             <h1 className="mt-5 text-5xl font-semibold">History</h1>
-            {isLoading || isFetching ? (
-                <CenterSpinner />
-            ) : (
-                <>
-                    <div className="mt-7 flex justify-between">
-                        <h5 className="text-sm text-gray-400 tracking-widest">
-                            {moment(workouts[0].start)
-                                .format("MMMM")
-                                .toUpperCase()}
-                        </h5>
-                        <h5 className="text-sm text-gray-400">
-                            {pagination.totalCount}{" "}
-                            {pagination.totalCount === 1
-                                ? "workout"
-                                : "workouts"}
-                        </h5>
-                    </div>
-                    <WorkoutHistoryList workouts={workouts} />
-                </>
-            )}
+            {isLoading || isFetching ? <CenterSpinner /> : content}
         </div>
     );
 };
